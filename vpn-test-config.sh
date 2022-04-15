4468b94004f4f24664f22bbc5976a0f5ded98f15
okamoto0730 <100657253+okamoto0730@users.noreply.github.com>
2022-04-15 17:52:37 +0900

    Update vpn-test-config.sh

--- vpn-test-config.sh
+++ vpn-test-config.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+#置き換えの値
+echo "\$1 = $1"
+
+
 #対象のディレクトリに移動する
 cd $HOME/easy-rsa
 
-
-
 #クライアント証明書を発行する
-./easyrsa build-client-full client.okamoto-kzk.test3 nopass
+./easyrsa build-client-full "$1" nopass
 
 #これはテストです
 test1=/c/Users/デスクトップ/github-remote/github-test
