7365af769ef0e6d3887fda5a7d7e5e357164e941
okamoto0730 <100657253+okamoto0730@users.noreply.github.com>
2022-05-20 12:47:39 +0900

    Update vpn-test-config.sh

--- vpn-test-config.sh
+++ vpn-test-config.sh
@@ -13,14 +13,24 @@ cd $HOME/easy-rsa
 ./easyrsa build-client-full "$1" nopass
 
 
+#発行されたcrt情報を、rcloneで同期済みのGoogleDriveにコピー。※rclone事前設定済みを想定。
+#参照URL：https://www.nextdoorwith.info/wp/se/how-to-control-google-drive-from-linux-rclone/　の「ファイルのアップロード・ダウンロード」項目
+
+#サーバー内でcrt情報が格納されているディレクトリ
+cd $HOME/easy-rsa/pki/issued/
+
+#対象のGoogleDriveにコピー。「dtest」は、rcloneとGoogleDriveの同期の際に設定した、name となります。
+rclone copy "$1".crt dtest:
+
+
 
 
 
 #これはテストです
-test1=/c/Users/デスクトップ/github-remote/github-test
+#test1=/c/Users/デスクトップ/github-remote/github-test
 
 #"〇〇"と、ダブルクォーテーション内の記述が実行される
-echo "cd $test1"
+#echo "cd $test1"
 
 #${〇〇}と、{}内の記述が実行される
-echo ${test1}_backup
+#echo ${test1}_backup
